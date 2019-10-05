f91c21968fd9a1c62504049bd54ad86ef2b746e4
Yeongmin Kim <oinochoe@naver.com>
2019-10-05 19:05:32 +0900

    image lazyloading

--- wall/js/wall3d.js
+++ wall/js/wall3d.js
@@ -40,5 +40,26 @@
     document.body.setAttribute("data-char", value);
   });
 
+  let images = [...document.querySelectorAll(".lazy")];
+
+  const interactSettings = {
+    root: document.querySelector(".house"),
+    rootMargin: "0px 0px 200px 0px"
+  };
+
+  function onIntersection(imageEntites) {
+    imageEntites.forEach(image => {
+      if (image.isIntersecting) {
+        observer.unobserve(image.target);
+        image.target.src = image.target.dataset.src;
+        image.target.onload = () => image.target.classList.add("loaded");
+      }
+    });
+  }
+
+  let observer = new IntersectionObserver(onIntersection, interactSettings);
+
+  images.forEach(image => observer.observe(image));
+
   resizeHandler();
 })();
